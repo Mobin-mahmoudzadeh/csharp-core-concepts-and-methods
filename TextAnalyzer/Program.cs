dca758b76056d07c1e05ce3b34e459efed55d24f
Mobin-mahmoudzadeh <mobin.mahmoudzadehhh@gmail.com>
2026-06-30 12:38:18 +0330

    feat: add TextAnalyzer project with string manipulation logic

--- TextAnalyzer/Program.cs
+++ TextAnalyzer/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿using System;
+
+namespace TextAnalyzer
+{
+    class Program 
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("=== Text Analyzer ===");
+            Console.Write("Please enter text to analyze: ");
+            
+            string text = Console.ReadLine(); 
+            
+            Console.WriteLine("\n------------------- Result -------------------");
+            Console.WriteLine("The length of the string (without spaces) is: " + text.Replace(" ", "").Length);
+            Console.WriteLine("Upper Case: " + text.ToUpper());
+            Console.WriteLine("Lower Case: " + text.ToLower());
+            
+            bool hasCSharp = text.Contains("C#");
+            Console.WriteLine("Does it contain 'C#'? " + hasCSharp);
+            Console.WriteLine("----------------------------------------------");
+        }
+    }
+}
